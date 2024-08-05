c1ef116e08b1f26bac9c535d6a2607dedb236f0a
RamonSilvaB <ramonsilvab05@gmail.com>
2024-08-05 15:08:10 -0300

    inserindo teste de comentarios com jest

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,35 @@
+/* eslint-disable testing-library/no-node-access */
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+import PostComments from '.';
 
 describe('Teste para o componente PostComment', () => {
     it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
+        render(<PostComments/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
+    });
+
+    test('Deve permitir adicionar dois comentários', () => {
+        render(<PostComments />);
+
+        const textarea = screen.getByTestId('comment-textarea');
+        const submitButton = screen.getByTestId('comment-submit-button');
+
+        // Adicionando o primeiro comentário
+        fireEvent.change(textarea, { target: { value: 'Primeiro comentário' } });
+        fireEvent.click(submitButton);
+
+        // Verificando se o primeiro comentário foi adicionado
+        expect(screen.getByText('Primeiro comentário')).toBeInTheDocument();
+
+        // Adicionando o segundo comentário
+        fireEvent.change(textarea, { target: { value: 'Segundo comentário' } });
+        fireEvent.click(submitButton);
+
+        // Verificando se o segundo comentário foi adicionado
+        expect(screen.getByText('Segundo comentário')).toBeInTheDocument();
+
+        // Verificando o número total de comentários
+        const commentsList = screen.getByTestId('comment-list');
+        expect(commentsList.children).toHaveLength(2);
     });
 });
